92a83ec16be23db4e1a8ff91b0412c69627f685e
georgiamoreira <georgiamoreira13@gmail.com>
2020-08-04 11:32:27 -0300

    TeacherForm and PageHeader

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import PageHeader from '../../components/PageHeader';
 
 
 function TeacherForm() {
   return (
-    <h1>Teachers Form</h1>
+    <div id="page-teacher-form" className="container">
+      <PageHeader title="Que incrível que você quer dar aulas!">
+        <form id="search-teachers">
+
+          <div className="input-block">
+            <label htmlFor="subject">Matéria</label>
+            <input type="text" id="subject"/>            
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="week_day">Dia da semana</label>
+            <input type="text" id="week_day"/>            
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="time">Hora</label>
+            <input type="text" id="time"/>            
+          </div>
+
+        </form>
+      </PageHeader>      
+    </div>
   )
 }
 
