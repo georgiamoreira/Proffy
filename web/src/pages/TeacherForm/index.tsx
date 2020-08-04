e48ad926a0d432e9eee6a34b4c1551d0b19e1146
georgiamoreira <georgiamoreira13@gmail.com>
2020-08-04 14:25:42 -0300

    TeacherList css

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -5,26 +5,7 @@ import PageHeader from '../../components/PageHeader';
 function TeacherForm() {
   return (
     <div id="page-teacher-form" className="container">
-      <PageHeader title="Que incrível que você quer dar aulas!">
-        <form id="search-teachers">
-
-          <div className="input-block">
-            <label htmlFor="subject">Matéria</label>
-            <input type="text" id="subject"/>            
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="week_day">Dia da semana</label>
-            <input type="text" id="week_day"/>            
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="time">Hora</label>
-            <input type="text" id="time"/>            
-          </div>
-
-        </form>
-      </PageHeader>      
+      <PageHeader title="Que incrível que você quer dar aulas!" />           
     </div>
   )
 }
