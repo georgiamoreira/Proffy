8f72838334d41d5195382315905ab2082df6bf4e
georgiamoreira <georgiamoreira13@gmail.com>
2020-08-06 22:18:45 -0300

    css

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -8,24 +8,27 @@ import './styles.css';
 
 interface PageHeaderProps {
   title: string;
+  description?: string;
 }
 
 const PageHeader: React.FC<PageHeaderProps> = (props) => {
   return (
     <header className="page-header">
-        <div className="top-bar-container">
-          <Link to="/">
-            <img src={backIcon} alt="Voltar" />
-          </Link>
-          <img src={logoImg} alt="Proffy" />
-        </div>
+      <div className="top-bar-container">
+        <Link to="/">
+          <img src={backIcon} alt="Voltar" />
+        </Link>
+        <img src={logoImg} alt="Proffy" />
+      </div>
 
-        <div className="header-content">
-          <strong>{props.title}</strong>
+      <div className="header-content">
+        <strong>{props.title}</strong>
+        { props.description && <p>{props.description}</p> }
 
-          {props.children}
-        </div>
-      </header>
+
+        {props.children}
+      </div>
+    </header>
   );
 }
 
--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -5,7 +5,22 @@ import PageHeader from '../../components/PageHeader';
 function TeacherForm() {
   return (
     <div id="page-teacher-form" className="container">
-      <PageHeader title="Que incrível que você quer dar aulas!" />           
+      <PageHeader 
+      title="Que incrível que você quer dar aulas!"
+      description="O primeiro passo é preencher o formulário de inscrição"      
+      />
+
+      <main>
+        <fieldset>
+          <legend>Seus dados</legend>
+
+          <div className="input-block">
+            <label htmlFor="subject">Matéria</label>
+            <input type="text" id="subject"/>            
+          </div>
+        </fieldset>
+      
+      </main>           
     </div>
   )
 }
