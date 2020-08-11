1372e69277051ed4356cdfdd68e7117984daff03
georgiamoreira <georgiamoreira13@gmail.com>
2020-08-10 23:58:34 -0300

    api TeacherList

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
 export interface Teacher {
     id: number;
@@ -18,9 +19,13 @@ interface TeacherItemProps {
   teacher: Teacher;
 }
 
-
-
 const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id,
+    })
+
+  }
   return (
     <article className="teacher-item">
       <header>
@@ -37,7 +42,7 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
           Preço/hora
               <strong>R$ {teacher.cost}</strong>
         </p>
-        <a href={`https://wa.me/${teacher.whatsapp}`}>
+        <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
           <img src={whatsappIcon} alt="whatsapp" />
           Entrar em contato
           </a>
